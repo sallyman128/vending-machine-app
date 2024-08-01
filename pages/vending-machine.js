8750577545fbbc8193a512a7b7717ba7c661c720
Salmaan Ali <salmaan.arif.ali@gmail.com>
2024-08-01 17:37:14 -0400

    used web3 to connect to wallet provider

--- pages/vending-machine.js
+++ pages/vending-machine.js
@@ -1,8 +1,27 @@
 import Head from "next/head";
+import { useState } from "react";
+import Web3 from "web3";
 import 'bulma/css/bulma.css'
 import styles from '../styles/VendingMachine.module.css'
 
-export default function VendingMachine() {
+const VendingMachine = () => {
+
+    const [error, setError] = useState("")
+    let web3;
+
+    const connectWalletHandler = async () => {
+        if (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') {
+            try {
+                await window.ethereum.request({method: "eth_requestAccounts"})
+                web3 = new Web3(window.ethereum)
+            } catch(err) {
+                setError(err.message)
+            }
+        } else {
+            console.log('please install metamask')
+        }
+    } 
+
     return (
         <div className={styles.main}>
             <Head>
@@ -16,7 +35,7 @@ export default function VendingMachine() {
                         <h1>Vending Machine</h1>
                     </div>
                     <div className="navbar-end">
-                        <button className="button is-primary">Connect Wallet</button>
+                        <button onClick={connectWalletHandler} className="button is-primary">Connect Wallet</button>
                     </div>
                 </div>
             </nav>
@@ -26,6 +45,13 @@ export default function VendingMachine() {
                     <p>placeholder text</p>
                 </div>
             </section>
+            <section>
+                <div className="container has-text-danger">
+                    <p>{error}</p>
+                </div>
+            </section>
         </div>
     )
 }
+
+export default VendingMachine
